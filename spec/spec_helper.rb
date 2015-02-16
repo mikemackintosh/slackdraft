7f0ed0c3d734cc65d1f45cbda242878b12a94762
Mike Mackintosh <m@zyp.io>
2015-02-16 17:36:45 -0500

    fixing rakefile encoding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,20 @@ require 'rspec/core'
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'slackdraft')
 
+# Create the share API context
+# so we can pass stuff between
+# the different tests
+RSpec.shared_context "shared environment", :a => :b do
+
+  before(:all) do
+
+    # Set webhook token
+    @slackhook = 'blahblahblah'
+
+  end
+
+end
+
 # Seems to run tests more than once if we do RSpec.configure more than once
 #unless RSpec.configuration.color_enabled == true
   RSpec.configure do |config|
