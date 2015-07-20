4025fe370f468750fdf5a0dc9741871bca897c0a
Mike Mackintosh <mike@signalsciences.com>
2015-07-20 11:10:37 -0400

    remove cli message

--- lib/slackdraft.rb
+++ lib/slackdraft.rb
@@ -11,12 +11,12 @@ require 'slackdraft/attachment'
 require 'slackdraft/format/init'
 
 module Slackdraft
-  
+
   def self.success(message)
-    puts " #{Paint['[✓]', :bright, :green]} #{Paint[message, :bright, :white]}"
+    puts " #{Paint['[ok]', :bright, :green]} #{Paint[message, :bright, :white]}"
     exit!
   end
-   
+
   def self.info(message)
     puts " #{Paint['[i]', :bright, :blue]} #{Paint[message, :bright, :white]}"
     exit!
@@ -26,7 +26,7 @@ module Slackdraft
     puts " #{Paint['[!]', :bright, :orange]} #{Paint[message, :bright, :white]}"
     exit!
   end
-  
+
   def self.fatal(message)
     puts " #{Paint['[x]', :bright, :red]} #{Paint[message, :bright, :white]}"
     exit!
